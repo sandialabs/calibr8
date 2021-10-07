ec7560889c586cb5598697fb2900a74fedae80f7
Brian Granzow <bngranz@sandia.gov>
2021-10-07 12:57:12 -0600

    Draft a portion of the surface mismatch qoi

--- src/surface_mismatch.cpp
+++ src/surface_mismatch.cpp
@@ -58,12 +58,70 @@ void SurfaceMismatch<T>::evaluate(
     int elem,
     RCP<GlobalResidual<T>> global,
     RCP<LocalResidual<T>> local,
-    apf::Vector3 const&,
+    apf::Vector3 const& iota_input,
     double,
     double) {
-  (void)elem_set;
-  (void)elem;
-  this->value_pt = 0. * global->vector_x(0)[0] + 0. * local->first_value();
+
+  // initialize the QoI contribution to 0
+  T const dummy1 = global->vector_x(0)[0];
+  T const dummy2 = local->first_value();
+  this->value_pt = 0 * (dummy1 + dummy2);
+
+  // get the id of the facet wrt element if this facet is on the QoI side
+  // do not evaluate if the facet is not adjacent to the QoI side
+  int const facet_id = m_mapping[elem_set][elem];
+  if (facet_id < 0) return;
+
+  // store some information contained in this class as local variables
+  int const ndims = this->m_num_dims;
+  apf::Mesh* mesh = this->m_mesh;
+  apf::MeshElement* mesh_elem = this->m_mesh_elem;
+
+  // grab the face to integrate over
+  apf::Downward elem_faces;
+  apf::MeshEntity* elem_entity = apf::getMeshEntity(mesh_elem);
+  mesh->getDownward(elem_entity, ndims - 1, elem_faces);
+  apf::MeshEntity* face = elem_faces[facet_id];
+
+  // get quadrature information over the face
+  int const q_order = 2;
+  apf::MeshElement* me = apf::createMeshElement(mesh, face);
+  int const num_qps = apf::countIntPoints(me, q_order);
+
+  // numerically integrate the QoI over the face
+  for (int pt = 0; pt < num_qps; ++pt) {
+
+    // get the integration point info on the face
+    apf::Vector3 iota_face;
+    apf::getIntPoint(me, q_order, pt, iota_face);
+    double const w = apf::getIntWeight(me, q_order, pt);
+    double const dv = apf::getDV(me, iota_face);
+
+    // map the integration point on the face parametric space to
+    // the element parametric space
+    apf::Vector3 iota_elem = boundaryToElementXi(
+        mesh, face, elem_entity, iota_face);
+
+    // interpolate the global variable solution to face integration point
+    // we assume displacements are index 0
+    int const disp_idx = 0;
+    global->interpolate(iota_elem);
+    Vector<T> const u_fem = global->vector_x(disp_idx);
+
+    // compute the difference between the FEM displacement and
+    // the measured input displacement data
+    (void)w;
+    (void)dv;
+
+  }
+
+  // clean up allocated memory
+  apf::destroyMeshElement(me);
+
+  // reset the state in global residual to what it was on input
+  global->interpolate(iota_input);
+
+
 }
 
 template class SurfaceMismatch<double>;
