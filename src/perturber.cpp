a7be9213a49eb2b56f58041a2a88b0184382de4d
Tom Seidl <dtseidl@sandia.gov>
2022-01-04 16:39:35 -0700

    update perturber for parallel meshes

--- src/perturber.cpp
+++ src/perturber.cpp
@@ -19,7 +19,7 @@ static void print_usage(int argc, char** argv) {
 
 class NormalNoise {
   public:
-    NormalNoise(unsigned int seed, double factor);
+    NormalNoise(int seed, double factor);
     double get_random();
   private:
     double m_factor;
@@ -27,8 +27,8 @@ class NormalNoise {
     std::normal_distribution<double> m_dist {0.0, 1.0};
 };
 
-NormalNoise::NormalNoise(unsigned int seed, double factor) {
-  m_mt = std::make_unique<std::mt19937>(seed);
+NormalNoise::NormalNoise(int seed, double factor) {
+  m_mt = std::make_unique<std::mt19937>(seed + PCU_Comm_Self());
   m_factor = factor;
 }
 
@@ -49,21 +49,19 @@ static void perturb_measured_data(
     NormalNoise& noise) {
   int const ndim = m->getDimension();
   auto f = get_step_data(m, step);
-  apf::MeshEntity* vtx;
-  auto it = m->begin(0);
-  while ((vtx = m->iterate(it))) {
-    apf::MeshElement* me = apf::createMeshElement(m, vtx);
-    apf::Element* fe = apf::createElement(f, me);
-    apf::NewArray<apf::Vector3> measured_data;
-    apf::getVectorNodes(fe, measured_data);
+  apf::Vector3 measured_vert;
+  apf::MeshEntity* vert;
+  apf::MeshIterator* verts = m->begin(0);
+  while ((vert = m->iterate(verts))) {
+    if (!m->isOwned(vert)) continue;
+    apf::getVector(f, vert, 0, measured_vert);
     for (int d = 0; d < ndim; ++d) {
-      measured_data[0][d] += noise.get_random();
+      measured_vert[d] += noise.get_random();
     }
-    apf::setVector(f, vtx, 0, measured_data[0]);
-    apf::destroyElement(fe);
-    apf::destroyMeshElement(me);
+    apf::setVector(f, vert, 0, measured_vert);
   }
-  m->end(it);
+  m->end(verts);
+  apf::synchronize(f);
 }
 
 int main(int argc, char** argv) {
@@ -73,9 +71,9 @@ int main(int argc, char** argv) {
   gmi_register_mesh();
   auto gfile = argv[1];
   auto mfile = argv[2];
-  auto nsteps = std::stoi(argv[3]);
-  unsigned int seed = std::stoi(argv[4]);
-  auto factor = std::stod(argv[5]);
+  int nsteps = std::stoi(argv[3]);
+  int seed = std::stoi(argv[4]);
+  double factor = std::stod(argv[5]);
   auto ofile = argv[6];
   auto mesh = apf::loadMdsMesh(gfile, mfile);
   mesh->verify();
