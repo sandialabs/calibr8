c0f6b0f96c81df770a3aaa617a0cb6f6675d10fa
Tom Seidl <dtseidl@sandia.gov>
2022-07-21 13:01:54 -0600

    fix bug with traction BCs in verify

--- src/main_verify.cpp
+++ src/main_verify.cpp
@@ -3,6 +3,7 @@
 #include <Teuchos_YamlParameterListHelpers.hpp>
 #include "adjoint.hpp"
 #include "control.hpp"
+#include "dbcs.hpp"
 #include "defines.hpp"
 #include "evaluations.hpp"
 #include "fields.hpp"
@@ -107,21 +108,39 @@ void Driver::solve_adjoint_fine() {
 void Driver::estimate_error() {
   print("ESTIMATING ERROR");
   apf::Mesh* m = m_nested->apf_mesh();
+  Array1D<RCP<VectorT>>& z = m_state->la->x[OWNED];
+  Array1D<RCP<VectorT>>& R = m_state->la->b[OWNED];
+  Array1D<RCP<VectorT>>& R_ghost = m_state->la->b[GHOST];
+  Array2D<RCP<MatrixT>>& dR_dx = m_state->la->A[OWNED];
   apf::Field* R_error = apf::createStepField(m, "R_error", apf::SCALAR);
   apf::Field* C_error = apf::createStepField(m, "C_error", apf::SCALAR);
   apf::zeroField(R_error);
   apf::zeroField(C_error);
   int const nsteps = m_nested->primal().size();
   ParameterList problem_params = m_params->sublist("problem", true);
-  ParameterList& tbcs = m_params->sublist("traction bcs");
   double const dt = problem_params.get<double>("step size");
+  ParameterList& tbcs = m_params->sublist("traction bcs");
+  ParameterList& dbcs = m_params->sublist("dirichlet bcs", true);
   double t = dt;
+  double e = 0.;
   for (int step = 1; step < nsteps; ++step) {
     print(" > at error step: %d", step);
     Array1D<apf::Field*> zfields = m_nested->adjoint(step).global;
+    m_state->la->resume_fill_A();
+    m_state->la->zero_all();
     eval_error_contributions(m_state, m_nested, R_error, C_error, step);
     eval_tbcs_error_contributions(tbcs, m_nested, zfields, R_error, t);
+    apply_primal_tbcs(tbcs, m_nested, R_ghost, t);
+    m_state->la->gather_b();
+    m_state->la->gather_x(/*sum=*/false);
+    apply_primal_dbcs(dbcs, m_nested, dR_dx, R, zfields, t, step,
+        /*is_adjoint=*/true);
+    t += dt;
+    for (int i = 0; i < m_state->residuals->global->num_residuals(); ++i) {
+      e += R[i]->dot(*(z[i]));
+    }
   }
+  print("eta ~ %.16e", e);
 }
 
 void Driver::evaluate_linearization_error() {
