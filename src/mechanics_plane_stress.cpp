4a79eb39e2656008f0f8418d98afab80c197e38a
Tom Seidl <dtseidl@sandia.gov>
2022-12-16 13:04:19 -0700

    fix finite deformation plane stress mechanics

--- src/mechanics_plane_stress.cpp
+++ src/mechanics_plane_stress.cpp
@@ -54,26 +54,27 @@ void MechanicsPlaneStress<T>::evaluate(
   // coupled ip set (lowest quadrature order)
   ALWAYS_ASSERT(ip_set == 0);
 
-  // gather variables from this residual quantities
-  Tensor<T> const grad_u = this->grad_vector_x(momentum_idx);
-
-  // compute kinematic quantities
-  Tensor<T> const I = minitensor::eye<T>(ndims);
-  Tensor<T> const F = grad_u + I;
-  Tensor<T> const F_inv = inverse(F);
-  Tensor<T> const F_invT = transpose(F_inv);
-  T const J = det(F);
-
   // compute stress measures
   RCP<GlobalResidual<T>> global = rcp(this, false);
   // Cauchy for these models is dev_cauchy
   Tensor<T> stress = local->dev_cauchy(global);
 
-  if (local->is_finite_deformation()) stress = J * stress * F_invT;
+  if (local->is_finite_deformation()) {
+    // gather variables from this residual quantities
+    Tensor<T> const grad_u = this->grad_vector_x(momentum_idx);
 
-  int const z_stretch_idx = local->z_stretch_idx();
-  T const z_stretch = local->scalar_xi(z_stretch_idx);
-  stress *= z_stretch;
+    // compute kinematic quantities
+    Tensor<T> const I = minitensor::eye<T>(ndims);
+    Tensor<T> const F = grad_u + I;
+    Tensor<T> const F_inv = inverse(F);
+    Tensor<T> const F_invT = transpose(F_inv);
+    T const J = det(F);
+
+    int const z_stretch_idx = local->z_stretch_idx();
+    T const z_stretch = local->scalar_xi(z_stretch_idx);
+
+    stress = z_stretch * J * stress * F_invT;
+  }
 
   // compute the balance of linear momentum residual
   for (int n = 0; n < nnodes; ++n) {
