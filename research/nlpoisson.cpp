38623ef48b4da93c3b807f5a6261b3df22f093e7
Brian Granzow <bngranz@sandia.gov>
2022-12-20 12:12:15 -0700

    update nlpoisson source term for mfctrd soln

--- research/nlpoisson.cpp
+++ research/nlpoisson.cpp
@@ -10,15 +10,15 @@ double eval_sin_exp_body_force(apf::Vector3 const& pt, double alpha) {
   double const x = pt[0];
   double const y = pt[1];
   static constexpr double pi = 3.14159265358979323;
-  return -2*alpha*(exp(x + y)*sin(2*pi*x)*sin(2*pi*y) + 1.0)*pow(sin(2*pi*x) +
-      2*pi*cos(2*pi*x), 2)*exp(2*x + 2*y)*pow(sin(2*pi*y), 2) -
-    2*alpha*(exp(x + y)*sin(2*pi*x)*sin(2*pi*y) + 1.0)*pow(sin(2*pi*y) +
-        2*pi*cos(2*pi*y), 2)*exp(2*x + 2*y)*pow(sin(2*pi*x), 2) -
-    (alpha*pow(exp(x + y)*sin(2*pi*x)*sin(2*pi*y) + 1.0, 2) + 1)*
-    (-4*pow(pi, 2)*sin(2*pi*x) + sin(2*pi*x) + 4*pi*cos(2*pi*x))*
-    exp(x + y)*sin(2*pi*y) -
-    (alpha*pow(exp(x + y)*sin(2*pi*x)*sin(2*pi*y) + 1.0, 2) + 1)*
-    (-4*pow(pi, 2)*sin(2*pi*y) + sin(2*pi*y) + 4*pi*cos(2*pi*y))*exp(x + y)*sin(2*pi*x);
+  return -(
+      2.*alpha*pow(sin(2.*pi*x) + 2.*pi*cos(2.*pi*x), 2.)*
+      exp(2.*x + 2.*y)*sin(2.*pi*x)*pow(sin(2.*pi*y), 3) +
+      2.*alpha*pow(sin(2.*pi*y) + 2.*pi*cos(2.*pi*y), 2.)*
+      exp(2.*x + 2.*y)*pow(sin(2.*pi*x), 3)*sin(2.*pi*y) +
+      (alpha*exp(2.*x + 2.*y)*pow(sin(2.*pi*x), 2.)*pow(sin(2.*pi*y), 2.) + 1.)*
+      (-4.*pow(pi, 2.)*sin(2.*pi*x) + sin(2.*pi*x) + 4.*pi*cos(2.*pi*x))*
+      sin(2.*pi*y) + (alpha*exp(2.*x + 2.*y)*pow(sin(2.*pi*x), 2.)*pow(sin(2.*pi*y), 2.) + 1.)*
+      (-4.*pow(pi, 2.)*sin(2.*pi*y) + sin(2.*pi*y) + 4.*pi*cos(2.*pi*y))*sin(2.*pi*x))*exp(x + y);
 }
 
 }
