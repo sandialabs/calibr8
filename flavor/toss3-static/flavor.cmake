b42dc2aa86762061e0e609e3ac443d4f67daf3b6
Tom Seidl <dtseidl@sandia.gov>
2024-06-27 15:03:58 -0600

    [build] add cee-static and toss3-static flavors
    removed Fortran dependence from Trilinos

--- flavor/toss3-static/flavor.cmake
+++ flavor/toss3-static/flavor.cmake
@@ -1,5 +1,4 @@
 set(CALIBR8_BUILD_SHARED_LIBS OFF)
-set(TRILINOS_EXTRA_LINK_FLAGS "-DTrilinos_EXTRA_LINK_FLAGS=dl")
 set(CALIBR8_ZLIB_PATH "${CAPP_INSTALL_ROOT}/zlib-ng/lib/libz.a")
 set(CALIBR8_ZOLTAN_PATH "${CAPP_INSTALL_ROOT}/trilinos/lib64/libzoltan.a")
 set(CALIBR8_METIS_PATH "${CAPP_INSTALL_ROOT}/parmetis/lib/libmetis.a")
