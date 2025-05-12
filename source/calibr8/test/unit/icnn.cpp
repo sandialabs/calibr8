f15a4996fcd347c55bcdec643c73494a8140f8fa
Brian Granzow <bngranz@sandia.gov>
2025-05-12 15:56:17 -0600

    [icnn] come up with an OK unit test for RAD<DFAD>

--- source/calibr8/test/unit/icnn.cpp
+++ source/calibr8/test/unit/icnn.cpp
@@ -4,19 +4,32 @@
 
 using namespace ML;
 
-TEST(input_convex_nn, dummy)
+TEST(input_convex_nn, does_rfad_dfad_work)
 {
+  DFADT yvar;
+  yvar = 1.0;
+  yvar.diff(0, 1);
+  Sacado::Rad::ADvar<DFADT> y = yvar;
+  auto const f = y*y*y;
+  Sacado::Rad::ADvar<DFADT>::Gradcomp();
+  auto const df = y.adj();
+  EXPECT_EQ(df.val(), 3);
+  EXPECT_EQ(df.fastAccessDx(0), 6);
+}
+
+TEST(input_convex_nn, scalar)
+{
+  using NETWORK = FICNN<RFAD_DFADT>;
   const char* activation = "softplus";
   std::vector<int> topology = {1,3,1};
-  /* TODO: segfault on construction
-     almost guaranteed due to indexing in setting up sizes */
-  //FICNN<RFAD_SFADT> network(activation, topology);
-
-  using NETWORK = FICNN<double>;
   NETWORK nn(activation, topology);
-
+  DFADT y_fad;
+  y_fad = 1.0;
+  y_fad.diff(0, 1);
+  Sacado::Rad::ADvar<DFADT> y_rad = y_fad;
   NETWORK::Vector y(1);
-  y(0) = 0.0;
-  nn.evaluate(y);
+  y(0) = y_rad;
 
+  /* segfaults */
+  //nn.evaluate(y);
 }
